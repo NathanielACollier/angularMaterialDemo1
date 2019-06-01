e978912fa713a05de7c95e11a7e9f101168122c2
Nathaniel Collier <nathaniel.collier@aecc.com>
2019-06-01 09:28:24 -0500

    node.js now serves angular.  Put the angular material theme in place

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,20 @@
 
 import express = require('express');
 import { generalController } from './controllers/general';
+var path = require('path');
 
 let port = 8080;
 const app: express.Application = express();
 
+/*
 app.get('/', (req,res)=>{
     res.send('Hello World!');
 });
+*/
 
 app.use('/general', generalController);
+
+app.use(express.static(path.join(__dirname, "../dist/angularApp")));
 
 app.listen(port, () => {
     console.log(`App listening on port: ${port}`);
